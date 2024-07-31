d904d19c28464f137ec22590642d79b56b205fee
AMAN SINGH <amansingh20032511@gmail.com>
2024-07-31 22:04:58 +0530

    Update ListProduct.jsx

--- admin/src/components/ListProduct/ListProduct.jsx
+++ admin/src/components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
   const [allProducts, setAllProducts] = useState([]);
 
   const fetchInfo = async () => {
-    await fetch("http://localhost:4000/allproducts")
+    await fetch("https://shopper-ht1j.onrender.com/allproducts")
       .then((resp) => resp.json())
       .then((data) => {
         setAllProducts(data);
@@ -18,7 +18,7 @@ const ListProduct = () => {
   }, []);
 
   const removeProduct = async (id) => {
-    await fetch("http://localhost:4000/removeproduct", {
+    await fetch("https://shopper-ht1j.onrender.com/removeproduct", {
       method: "POST",
       headers: {
         Accept: "application/json",
